6efec6687a0a2839f260569aae0c47acd524cead
rameno0ds <ranen.tan.2023@scis.smu.edu.sg>
2025-12-02 21:37:45 +0800

    ex2

--- ex2/ex2_unit_tests.py
+++ ex2/ex2_unit_tests.py
@@ -1,21 +1,24 @@
 import unittest
-from ex2_smelly import Shape  # Adjust if the class is in another file
+from ex2_smelly import Shape, Circle, Rectangle, Triangle # Adjust if the class is in another file
 
 class TestShape(unittest.TestCase):
 
     def test_calculate_area_circle(self):
         # Test for a circle with radius 5
-        result = Shape().calculate_area('circle', radius=5)
+        shape = Circle()
+        result = shape.calculate_area(radius=5)
         self.assertAlmostEqual(result, 78.5, places=2)
 
     def test_calculate_area_rectangle(self):
         # Test for a rectangle with length 4 and width 6
-        result = Shape().calculate_area('rectangle', length=4, width=6)
+        shape = Rectangle()
+        result = shape.calculate_area(length=4, width=6)
         self.assertEqual(result, 24)
 
     def test_calculate_area_triangle(self):
         # Test for a triangle with base 10 and height 8
-        result = Shape().calculate_area('triangle', base=10, height=8)
+        shape = Triangle()
+        result = shape.calculate_area(  base=10, height=8)
         self.assertEqual(result, 40)
 
 
